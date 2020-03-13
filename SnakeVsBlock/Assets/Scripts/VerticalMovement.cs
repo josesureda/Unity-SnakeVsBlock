381bf6894c4f5cae2d05babdc2c0a77bff19de32
josesureda <suredajose@gmail.com>
2020-03-13 05:57:46 -0300

    Add Level Mockup with vertical movement

---
+++ SnakeVsBlock/Assets/Scripts/VerticalMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f; 
+
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector2.up * speed * Time.deltaTime);
+        
+        
+    
+        
+    }
+}
