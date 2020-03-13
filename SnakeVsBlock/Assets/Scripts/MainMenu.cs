e4c2545e6d0adb6a880e118b829988041a6438ba
josesureda <suredajose@gmail.com>
2020-03-13 05:51:30 -0300

    Add Main Menu Script

---
+++ SnakeVsBlock/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Load Level Scene on player input
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Level");
+        }
+    }
+}
